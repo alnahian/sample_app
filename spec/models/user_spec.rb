c7b052c03b34c8d67e69f7d521cab34720c9e828
alnahian <nahian33@yahoo.com>
2013-10-01 19:31:11 +0600

    show new micropost form with previous microposts(proto-feed),proto-feed user spec,user model feed,static page spec feed,static page controller feed,feed and feed item partial for home page,micropost controller,destroying micropost,delete link added to micropost partial,feed item partial,micropost pages spec,micropost controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,7 @@ describe User do
   it { should respond_to(:authenticate) }
   
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
   
   it { should respond_to(:name) }
   it { should respond_to(:email) }
@@ -43,6 +44,8 @@ describe User do
     it { should be_admin }
   end
 
+  
+  
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -161,6 +164,16 @@ describe User do
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
       end
+	end
+	
+	describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
     end
   end
   
